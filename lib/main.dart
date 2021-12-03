113fbc9e671c9c42bd83b17c0a91acd70db49734
Sherange <sherangef@gmail.com>
2021-12-04 00:31:02 +0530

    set answers

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,32 @@ class _QuizPageState extends State<QuizPage> {
     'Some cats are actually allergic to humans',
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+    'A slug\'s blood is green.',
+    'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+    'It is illegal to pee in the Ocean in Portugal.',
+    'No piece of square dry paper can be folded in half more than 7 times.',
+    'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+    'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+    'The total surface area of two human lungs is approximately 70 square metres.',
+    'Google was originally called \"Backrub\".',
+    'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+    'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+  ];
+
+  List<bool> answers = [
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true
   ];
 
   int questionNumber = 0;
@@ -67,13 +92,24 @@ class _QuizPageState extends State<QuizPage> {
                   backgroundColor: MaterialStateProperty.all(Colors.green),
                 ),
                 onPressed: () {
-                  setState(() {
-                    questionNumber++;
-                    scoreKeeper.add(const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ));
-                  });
+                  bool correctAnswer = answers[questionNumber];
+                  if (correctAnswer == true) {
+                    setState(() {
+                      questionNumber++;
+                      scoreKeeper.add(const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ));
+                    });
+                  } else {
+                    setState(() {
+                      questionNumber++;
+                      scoreKeeper.add(const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ));
+                    });
+                  }
                 },
                 child: const Text(
                   'True',
@@ -90,13 +126,24 @@ class _QuizPageState extends State<QuizPage> {
                   backgroundColor: MaterialStateProperty.all(Colors.red),
                 ),
                 onPressed: () {
-                  setState(() {
-                    questionNumber++;
-                    scoreKeeper.add(const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ));
-                  });
+                  bool correctAnswer = answers[questionNumber];
+                  if (correctAnswer == false) {
+                    setState(() {
+                      questionNumber++;
+                      scoreKeeper.add(const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ));
+                    });
+                  } else {
+                    setState(() {
+                      questionNumber++;
+                      scoreKeeper.add(const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ));
+                    });
+                  }
                 },
                 child: const Text(
                   'False',
